436337c05c459c0a3b65f70a5cc45847ecbb1f58
RonnieKing <jinxuerong@gmail.com>
2025-01-01 14:23:47 -0500

    change cuda to mps

--- train.py
+++ train.py
@@ -188,7 +188,8 @@ if __name__ == '__main__':
     args = get_args()
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') #CUDA
+    device = torch.device('mps' if torch.mps.is_available() else 'cpu')
     logging.info(f'Using device {device}')
 
     # Change here to adapt to your data
